71ad7ba402d41ea55e9cd28112429a41432513f8
2601110294-a11y <2601110294@gspace.kopo.ac.kr>
2026-05-28 11:23:22 +0900

    java Continue Break While

--- src/ai0521/Rabbit/Rabbit.java
+++ src/ai0521/Rabbit/Rabbit.java
@@ -5,6 +5,8 @@ public class Rabbit {
     int xPos;// x위치
     int yPos;// y위치
 
+    //jvm은 클래스 내에 생성사가 하나도 없는 경우 기본생성자를 만들어서 사용한다
+    // 그러나 클래스 내에 생성자가 선언 되어 있다면 선언된 생성자만  사용할수있다
     public Rabbit(){
     }
 
@@ -18,6 +20,7 @@ public class Rabbit {
     public void setShape(String shape) {
         this.shape = shape;
     }
+
 
     public int getxPos() {
         return xPos;
@@ -34,7 +37,28 @@ public class Rabbit {
     public void setyPos(int yPos) {
         this.yPos = yPos;
     }
-}
+
+    //토까의 좌표
+    public void setLocation(int xPos, int yPos){
+        this.yPos = yPos;
+        this.xPos = xPos;
+    }
+    //토끼의 모양,위치  출력 메소드
+    public void printInfo() {
+        System.out.printf("%s 모양의 토끼는 (%d,%d) 좌표에 위치하고 있다.\n", shape, xPos, yPos);
+    }
+        //x축으로 10씩 오른쪽으로 이동하는 메소드
+        public void moveRightTen(){
+            xPos += 10;
+        }
+
+        //y축으로 10씩 왼쪽으로 이동하는 메소드
+        public void moveLeftTen(){
+            yPos += 10;
+        }
+
+
+    }
 
 
 
