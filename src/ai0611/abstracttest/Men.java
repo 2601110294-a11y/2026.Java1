6d202ca57f7234580acacbc587f7879b8d56ac7c
2601110294-a11y <2601110294@gspace.kopo.ac.kr>
2026-06-11 11:46:23 +0900

    추상클래스 인터페이스 공부

---
+++ src/ai0611/abstracttest/Men.java
@@ -0,0 +1,8 @@
+package ai0611.abstracttest;
+
+public class Men extends Person{
+    @Override
+    public void study() {
+        System.out.println("아들이 공부를 열심히 한다.");
+    }
+}
---
+++ src/ai0611/abstracttest/RunPerson.java
@@ -0,0 +1,27 @@
+package ai0611.abstracttest;
+
+public class RunPerson {
+    public static void main(String[] args) {
+
+        Men men = new Men();
+        men.name = "김폴리";
+        men.weight =70;
+        men.height = 180;
+
+
+        Women women = new Women();
+        women.name = "박인순";
+        women.weight =40;
+        women.height = 170;
+
+        men.eat("김치찌개");
+        men.study();
+        men.sleep(8);
+
+        women.eat("파스타");
+        women.study();
+        women.sleep(10);
+
+    }
+
+}
